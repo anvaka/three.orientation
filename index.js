48ec0740f603b8f197be846419d8661c7fee1a2d
anvaka <anvaka@gmail.com>
2014-11-09 01:02:31 -0800

    Preserve original object rotation

--- index.js
+++ index.js
@@ -3,9 +3,11 @@
  * @author richt / http://richt.me
  * @author WestLangley / http://github.com/WestLangley
  *
- * @author Andrei Kashcha / https://github.com/anvaka - adopted to commonjs,
- * restructured to avoid `this`
- * Original source code is here: http://threejs.org/examples/js/controls/DeviceOrientationControls.js
+ * @author Andrei Kashcha / https://github.com/anvaka:
+ * + adopted to commonjs,
+ * + restructured to avoid `this`
+ * + preserve original object rotation
+ * + original source code is here: http://threejs.org/examples/js/controls/DeviceOrientationControls.js
  *
  * W3C Device Orientation control (http://w3c.github.io/deviceorientation/spec-source-orientation.html)
  */
@@ -20,6 +22,7 @@ function orientationControl(object) {
   var deviceOrientation = {};
   var screenOrientation = 0;
 
+  var originalRotation;
   var setObjectQuaternion = createObjectQuaterionSetter();
 
   connect();
@@ -27,10 +30,13 @@ function orientationControl(object) {
   return {
     update: update,
     connect: connect,
-    disconnect: disconnect
+    disconnect: disconnect,
   };
 
   function connect() {
+    deviceOrientation = {};
+    originalRotation = object.quaternion.clone();
+
     onScreenOrientationChangeEvent(); // run once on load
     window.addEventListener('orientationchange', onScreenOrientationChangeEvent, false);
     window.addEventListener('deviceorientation', onDeviceOrientationChangeEvent, false);
@@ -47,7 +53,7 @@ function orientationControl(object) {
   function update() {
     if (freeze) return;
 
-    var alpha = deviceOrientation.gamma ? THREE.Math.degToRad(deviceOrientation.alpha) : 0; // Z
+    var alpha = deviceOrientation.alpha ? THREE.Math.degToRad(deviceOrientation.alpha) : 0; // Z
     var beta = deviceOrientation.beta ? THREE.Math.degToRad(deviceOrientation.beta) : 0; // X'
     var gamma = deviceOrientation.gamma ? THREE.Math.degToRad(deviceOrientation.gamma) : 0; // Y''
     var orient = screenOrientation ? THREE.Math.degToRad(screenOrientation) : 0; // O
@@ -64,17 +70,22 @@ function orientationControl(object) {
   }
 
   function createObjectQuaterionSetter() {
-    // The angles alpha, beta and gamma form a set of intrinsic Tait-Bryan angles of type Z-X'-Y''
-    var zee = new THREE.Vector3(0, 0, 1);
-    var euler = new THREE.Euler();
-    var q0 = new THREE.Quaternion();
-    var q1 = new THREE.Quaternion(-Math.sqrt(0.5), 0, 0, Math.sqrt(0.5)); // - PI/2 around the x-axis
+    var deviceEuler = new THREE.Euler();
+    var deviceQuaternion = new THREE.Quaternion();
+    var screenTransform = new THREE.Quaternion();
+    var worldTransform = new THREE.Quaternion(-Math.sqrt(0.5), 0, 0, Math.sqrt(0.5)); // - PI/2 around the x-axis
+    var minusHalfAngle = 0;
 
     return function(quaternion, alpha, beta, gamma, orient) {
-      euler.set(beta, alpha, -gamma, 'YXZ'); // 'ZXY' for the device, but 'YXZ' for us
-      quaternion.setFromEuler(euler); // orient the device
-      quaternion.multiply(q1); // camera looks out the back of the device, not the top
-      quaternion.multiply(q0.setFromAxisAngle(zee, -orient)); // adjust for screen orientation
+      quaternion.copy(originalRotation);
+      deviceEuler.set(beta, alpha, -gamma, 'YXZ'); // 'ZXY' for the device, but 'YXZ' for us
+      quaternion.multiply(deviceQuaternion.setFromEuler(deviceEuler)); // orient the device
+
+      minusHalfAngle = -orient / 2;
+      screenTransform.set( 0, Math.sin( minusHalfAngle ), 0, Math.cos( minusHalfAngle ) );
+
+      quaternion.multiply(screenTransform); // adjust for screen orientation
+      quaternion.multiply(worldTransform);  // camera looks out the back of the device, not the top
     };
   }
 }
